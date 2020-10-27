e8981c47d6e42833601a5654102a9c37be023ad9
Leonan Pereira Pinto <leonan@imaginationmedia.com>
2020-10-26 22:18:21 -0300

    Adding a Global Expection Handler

--- LifeBackup.Api/Startup.cs
+++ LifeBackup.Api/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using Amazon.S3;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace LifeBackup.Api
 {
@@ -32,6 +34,16 @@ namespace LifeBackup.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseExceptionHandler(a => a.Run(async context =>
+            {
+                var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                var exception = exceptionHandlerPathFeature.Error;
+
+                var result = JsonConvert.SerializeObject(new {error = exception.Message});
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(result);
+            }));
 
             app.UseMvc();
 
