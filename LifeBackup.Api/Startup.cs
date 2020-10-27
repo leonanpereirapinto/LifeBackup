bf10a2c13a9fa3f78b98b0738830bce81d75442e
Leonan Pereira Pinto <leonan@imaginationmedia.com>
2020-10-26 22:12:21 -0300

    Creating Amazon S3 Client

--- LifeBackup.Api/Startup.cs
+++ LifeBackup.Api/Startup.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Amazon.S3;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LifeBackup.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAWSService<IAmazonS3>(_configuration.GetAWSOptions());
+
             services.AddMvc();
         }
 
