0064517e6b3390d3ceca0891db1012e1586a86da
Leonan Pereira Pinto <leonan@imaginationmedia.com>
2020-11-11 20:25:10 -0300

    Setting up the WebApplicationFactory

---
+++ LifeBackup.Integration.Tests/Scenarios/FilesControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+using Amazon.S3;
+using LifeBackup.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace LifeBackup.Integration.Tests.Scenarios
+{
+    [Collection("api")]
+    public class FilesControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public FilesControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddAWSService<IAmazonS3>(new AWSOptions
+                    {
+                        DefaultClientConfig =
+                        {
+                            ServiceURL = "http://localhost:9003"
+                        },
+                        Credentials = new BasicAWSCredentials("FAKE", "FAKE")
+                    });
+                });
+            }).CreateClient();
+        }
+    }
+}
